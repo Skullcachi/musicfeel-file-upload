721267f4e835e90b69211f733c092f2bf9ebdd9e
Francisco Solís <josusols@gmail.com>
2019-07-11 11:53:39 -0600

    app.user to app.use
    typo

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.user(cors());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
